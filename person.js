022101f8d270448e3aad6fb579f361b1ef835cb3
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-04-19 18:58:37 +0300

    feat:noi updateuri

--- person.js
+++ person.js
@@ -1,14 +1,14 @@
 function calculate() {
-  // preluăm valorile introduse de utilizator
+  
   const nume = document.getElementById("input01").value;
   const gen = document.getElementById("input02").value;
   const greutate = parseFloat(document.getElementById("input03").value);
   const inaltime = parseFloat(document.getElementById("input04").value) / 100; //convertim in cm in metri
   
-  // calculăm indicele de masă corporală
+
   const imc = greutate / (inaltime * inaltime);
 
-  // afișăm rezultatul în elementul cu id-ul "answer"
+ 
   const answer = document.getElementById("answer");
   answer.innerHTML = `${nume} (${gen}) IMC: ${imc.toFixed(2)}`;
 }
